0f2b927263549862750fb5765686a28e93cc9953
jibeken <kenjibek1606@icloud.com>
2024-10-17 22:35:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let x = 10;
+let y = 5;
+
+// Сложение
+console.log( x + y);
+
+// Вычитание
+console.log( x - y);
+
+// Умножение
+console.log( x * y);
+
+// Деление
+console.log(x / y);
+
+// Остаток от деления
+console.log(x % y);
+
+// Возведение в степень
+console.log(x ** y);
